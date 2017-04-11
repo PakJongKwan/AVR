fcb59bbf2ebcbd6491a6d63cc08da00201fa1be5
matt1023 <pakjongkwan@gmail.com>
2017-04-12 03:28:39 +0900

    Decoder Ver.2

--- Decoder/DECODER.c
+++ Decoder/DECODER.c
@@ -1,6 +1,15 @@
-//74HC139
-//74LS373
-//74LS04P
+/*************************************
+*2017/04/12                          *
+*Multiplexer Counter                 *
+**************************************
+*Used IC                             *
+-------------------------------------*
+*decoder/demultiplexer - 74HC139     *
+*octal transparent latch = 74LS373   *
+*not gate - 74LS04P                  *
+-------------------------------------*
+**************************************/
+
 #include <io.h>
 #include <delay.h>
 unsigned char FND[] = {0xc0,0xf9,0xa4,0x30,0x99,0x92,0x82,0xf8,0x80,0x90};
@@ -12,19 +21,21 @@ void out(char addr, char data){
     PORTE &= 0xfb;
     PORTE |= 0x04;
 }
-    
+
 void main(void){
     int a;
     int b =0;
     DDRA = 0xff;
     DDRE = 0xff;
     while(1){
-        for(a=0;a<7;a++){
+        for(a=0;a<7;a++)
+        {
             out(0x00, LEDL<<a);
             delay_ms(500);
-            
         }
+        //When seven LEDs Turn on
         out(0x01, FND[b]);
+        //FND count
         b++;
         delay_ms(500);
     }
